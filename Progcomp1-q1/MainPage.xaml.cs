fa83a77033a63667719208432d2686cc06596967
Nathan Wong <nathan@nathanwong.co.uk>
2013-10-15 18:56:02 +0100

    rot13

--- Progcomp1-q1/MainPage.xaml.cs
+++ Progcomp1-q1/MainPage.xaml.cs
@@ -34,5 +34,55 @@ namespace Progcomp1_q1
         protected override void OnNavigatedTo(NavigationEventArgs e)
         {
         }
+
+        /***
+         * Invoked when the cipher text is being changed. Should rot(12) cipher
+         * and put output in clearText
+         **/
+        private void cipherText_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            clearText.Text = Alien.Decode(cipherText.Text);
+        }
+
+        public class Alien
+        {
+            public static string Decode(string cipher)
+            {
+                char[] chars = cipher.ToCharArray();
+
+                for (int i = 0; i < chars.Length; i++)
+                {
+                    int letter = (int)chars[i];
+
+                    if (letter >= 'a' && letter <= 'z')
+                    {
+
+                        if (letter > 'l')
+                        {
+                            letter -= 12;
+                        }
+                        else
+                        {
+                            letter += 13;
+                        }
+                    }
+                    else if (letter >= 'A' && letter <= 'z')
+                    {
+                        if (letter > 'M')
+                        {
+                            letter -= 13;
+                        }
+                        else
+                        {
+                            letter += 13;
+                        }
+                    }
+
+                    chars[i] = (char)letter;
+                }
+
+                return new string(chars);
+            }
+        }
     }
 }
